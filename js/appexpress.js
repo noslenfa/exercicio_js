0ff24a7a0aca3f52af4e464daa8b461b71487678
Nelson Amaral <noslenfa@gmail.com>
2015-06-30 23:11:43 +0100

    appexpress.js changed added content from apprequest.js

--- js/appexpress.js
+++ js/appexpress.js
@@ -1,16 +1,19 @@
 var express = require('express');
+var request = require('request');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/musics', function (req, res) {
+	res.send();
 });
 
 var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+	var host = server.address().address;
+	var port = server.address().port;
+  	// console.log('Example app listening at http://%s:%s', host, port);
 });
 
+request('https://raw.githubusercontent.com/noslenfa/exvasco/master/musics.json', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body)
+  }
+});
