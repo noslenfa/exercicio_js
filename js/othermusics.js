c15e5ba14a37c59890c6d2c0f6adb56e0cd87b8f
Nelson Amaral <noslenfa@gmail.com>
2015-07-05 23:51:22 +0100

    added recursivity to othermusics.js

--- js/othermusics.js
+++ js/othermusics.js
@@ -1,9 +1,8 @@
-var j=0;
+var i=0;
 var final_url = "http://localhost:8000/othermusics";
 $.get(final_url).done( function(data) {
             var jsondata = JSON.parse(data);
             var jsonaTracks = jsondata.aTracks;
-            //console.log(jsondata.aTracks[10].track_title);
             for (var i = 0 ; i < jsonaTracks.length ; i++){
                 show_html(jsonaTracks, i)
             };
@@ -11,7 +10,20 @@ $.get(final_url).done( function(data) {
 
 
 var show_html = function (jsonaTracks, i) {
-	//console.log(jsondata.aTracks[10].track_title);
     $(".othermusics").append("<tr><td>" + jsonaTracks[i].artist_name + "</td><td>" + jsonaTracks[i].track_title + "</td><td>" + jsonaTracks[i].album_title + "</td></tr>");
+    i++;
 }
 
+var getData = function (i) {
+
+    $.get(final_url).done( function(jsonaTracks) {
+        var final_url = "http://localhost:8000/othermusics";
+        if (jsonaTracks.artist_name !== undefined && jsonaTracks.track_title !== undefined && jsonaTracks.album_title !== undefined) {
+            show_html(jsonaTracks);
+            i++;
+        } 
+        getData(i);
+    }).fail( function(error){
+        getData(i);
+    });
+}
