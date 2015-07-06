8fc299e25d636b1cec4feb81d74285a1fb7f0d46
Nelson Amaral <noslenfa@gmail.com>
2015-07-06 10:55:07 +0100

    back to las othermusics.js

--- js/othermusics.js
+++ js/othermusics.js
@@ -1,5 +1,5 @@
-var i=0;
 var final_url = "http://localhost:8000/othermusics";
+
 $.get(final_url).done( function(data) {
             var jsondata = JSON.parse(data);
             var jsonaTracks = jsondata.aTracks;
@@ -11,19 +11,5 @@ $.get(final_url).done( function(data) {
 
 var show_html = function (jsonaTracks, i) {
     $(".othermusics").append("<tr><td>" + jsonaTracks[i].artist_name + "</td><td>" + jsonaTracks[i].track_title + "</td><td>" + jsonaTracks[i].album_title + "</td></tr>");
-    i++;
-}
-
-var getData = function (i) {
-
-    $.get(final_url).done( function(jsonaTracks) {
-        var final_url = "http://localhost:8000/othermusics";
-        if (jsonaTracks.artist_name !== undefined && jsonaTracks.track_title !== undefined && jsonaTracks.album_title !== undefined) {
-            show_html(jsonaTracks);
-            i++;
-        } 
-        getData(i);
-    }).fail( function(error){
-        getData(i);
-    });
+    //i++;
 }
