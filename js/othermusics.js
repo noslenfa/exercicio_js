9523ffba7479cbfd334e7c9e5e7c8cb68f0caef8
Nelson Amaral <noslenfa@gmail.com>
2015-07-06 14:01:51 +0100

    othermusics.js changed

--- js/othermusics.js
+++ js/othermusics.js
@@ -1,15 +1,42 @@
-var final_url = "http://localhost:8000/othermusics";
-
-$.get(final_url).done( function(data) {
+//var final_url = "http://localhost:8000/othermusics";
+//var otheri;
+/*$.get(final_url).done( function(data) {
             var jsondata = JSON.parse(data);
             var jsonaTracks = jsondata.aTracks;
             for (var i = 0 ; i < jsonaTracks.length ; i++){
                 show_html(jsonaTracks, i)
             };
-});
+});*/
+
+
+/*$.get(final_url).done( function(data) {
+            var jsondata = JSON.parse(data);
+            var jsonaTracks = jsondata.aTracks;
+            var i = jsonaTracks.length;
+            console.log(i);
+});*/
 
 
 var show_html = function (jsonaTracks, i) {
     $(".othermusics").append("<tr><td>" + jsonaTracks[i].artist_name + "</td><td>" + jsonaTracks[i].track_title + "</td><td>" + jsonaTracks[i].album_title + "</td></tr>");
-    //i++;
+    i--;
+}
+
+var getData = function (i) {
+
+            var final_url = "http://localhost:8000/othermusics";
+            $.get(final_url).done( function(data) {
+                var jsondata = JSON.parse(data);
+                var jsonaTracks = jsondata.aTracks;
+                var i = jsonaTracks.length;
+                if ( i >= 0 ) {
+                    console.log(i);
+                    show_html(jsonaTracks, i);
+                }
+                console.log(i);
+                getData(i);
+                i--;
+            });
+     
+
 }
